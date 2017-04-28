fd554b963ea45135fb94a608def22b4cc1ce69db
Antoine-Gelgon <antoine.gelgon@gmail.com>
2017-04-28 10:56:50 +0200

    First TTF and SFD

--- ume-stroke-stroke/generate.sh
+++ ume-stroke-stroke/generate.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-string=$(cat input.txt)
-ii=0
-while printf "%s%n" "${string:ii++:1}" xx; do
-	((xx)) && printf "'," || break
-done > output.txt
+
+for gly in svg/simple/*.svg;
+do
+
+  file=`basename $gly .svg`
+  cp $gly svg/simple/clean/
+  inkscape \
+    --verb EditSelectAllInAllLayers \
+    --verb SelectionUnGroup \
+    --verb StrokeToPath \
+    --verb SelectionUnion \
+    --verb FileSave \
+    --verb FileClose \
+    --verb FileQuit \
+  svg/simple/clean/$file.svg
+
+  # echo $file
+  # inkscape $gly --export-ps="simple/$file.ps"
+  # # inkscape "simple/$file.eps" --export-plain-svg="simple/$file.svg"
+  # # rm simple/$file.esp
+done
 
 
--- ume-stroke-stroke/importSVG.py
+++ ume-stroke-stroke/importSVG.py
@@ -4,27 +4,23 @@ import fontforge
 import glob
 import sys
 
-SVG_DIR = glob.glob('svg/*.svg')
-font = fontforge.open('tmp/empty.sfd')
+SVG_DIR = glob.glob('svg/simple/clean/*.svg')
+font = fontforge.open('tmp/ume-gothic.sfd')
 
 for glyph in SVG_DIR:
-    print(glyph)
-    svg_file = open(glyph, "r")
-    for ligne in svg_file:
-        print(ligne)
-    # for ligne in svg_file:
-    #     if line_chasse in ligne:
-    #         val_chasse = ligne.split('width="')[-1].split('" height="')[-2]
-    #         val_chasse = (float(val_chasse) / 2) / 1.075
-    #         # print('la valeur des de', val_chasse)
-    # svg_file.close()
+    letter = glyph.split("/")[-1].replace(".svg", "")
+    print(letter)
+    zone = font[letter]
+    chasse = zone.width
+    zone.clear()
+    letter_char = font.createMappedChar(letter)
+    letter_char.width = chasse
+    letter_char.importOutlines(glyph)
 
 #     letter = glyph.split("/")[-1].replace(".svg", "")
 #     letter_char= font.createMappedChar(letter)
 #     letter_char.width = 600
 #     letter_char.importOutlines(glyph)
-#
-#
-# font.fontname = 'ume-stroke-stroke'
-# font.familyname = 'ume-stroke-stroke'
-# font.generate('FINAL/ume.sfd')
+font.fontname = 'ume-stroke-stroke'
+font.familyname = 'ume-stroke-stroke'
+font.generate('FINAL/ume-stroke-stroke-v1.sfd')
