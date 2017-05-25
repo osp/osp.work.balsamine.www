3fa0aa1956f1718d841034af44754f887c84b810
Antoine-Gelgon <antoine.gelgon@gmail.com>
2017-05-25 10:46:15 +0200

    new version umeStrokeStroke

---
+++ ume-stroke-stroke/no-cond/ext.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+inkscape --verb EditSelectAllInAllLayers \
+         --verb ObjectRotate20 \
+         --verb FileSave \
+         --verb FileClose \
+         --verb FileQuit \
+a.svg
+
+
