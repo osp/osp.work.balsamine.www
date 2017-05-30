2eecc2feaebc15c42ddc8093b3ff9df362b599c6
Alex Leray <alexandre@stdin.fr>
2017-05-30 23:20:34 +0200

    Sorted out how to sort css

--- bin/cssfmt.sh
+++ bin/cssfmt.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
-./node_modules/.bin/postcss --use stylefmt --use postcss-sorting --postcss-sorting.sort-order yandex --use postcss-single-line --no-map --output theme/balsamine/static/css/balsa.css theme/balsamine/static/css/balsa.css
+# ./node_modules/.bin/postcss --use stylefmt --use postcss-sorting --postcss-sorting.sort-order yandex --use postcss-single-line --no-map --output theme/balsamine/static/css/balsa.css theme/balsamine/static/css/balsa.css
+./node_modules/.bin/postcss --no-map --replace theme/balsamine/static/css/balsa.css
