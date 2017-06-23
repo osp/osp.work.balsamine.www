9d01fab36dcfc480a65a26e72356a870178353c6
Antoine-Gelgon <antoine.gelgon@gmail.com>
2017-06-23 13:47:05 +0200

    fond bouton location

--- ume-stroke-stroke/test.py
+++ ume-stroke-stroke/test.py
@@ -7,15 +7,25 @@ from math import radians
 FontName = sys.argv[1]
 SVG_DIR = glob.glob('SVG-GENERATED/'+FontName+'/*.svg')
 font = fontforge.open('tmp/ume-gothic.sfd')
+line_chasse = '<svg '
 
 for glyph in SVG_DIR:
     letter = glyph.split("/")[-1].replace(".svg", "")
     print(letter)
+    svg_file = open(glyph, "r")
+    for ligne in svg_file:
+        if line_chasse in ligne:
+            chasse = ligne.split('width="')[-1].split('" height="')[-2]
+            # val_chasse = (float(val_chasse) / 2) / 1.075
+            print('la valeur des de', chasse)
+    svg_file.close()
+
+
     zone = font[letter]
-    chasse = zone.width
+    # chasse = zone.width
     zone.clear()
     letter_char = font.createMappedChar(letter)
-    letter_char.width = chasse
+    letter_char.width = int(chasse)
     letter_char.importOutlines(glyph)
     # letter_char.stroke("eliptical",60,40,radians(-30.0),"square","round")
     # letter_char.stroke("circular", 1 , 'butt' , 'miter' ,'cleanup')
